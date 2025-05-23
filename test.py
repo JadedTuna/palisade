de683c1c77af2c5d6b77eba5ab85c172a6d0f464
ALHANKO <ludvig.alhanko.kth@gmail.com>
2025-05-23 11:59:03 +0200

    Small Update to Tests

--- test.py
+++ test.py
@@ -19,8 +19,8 @@ def run_tests():
     indent = ' '*4
     border_length = 80
     for index, test in enumerate(test_order):
-        start_border_length = border_length - len(str(index+1))
-        end_border_length = start_border_length + 3
+        start_border_length = border_length-len(str(index+1))
+        end_border_length = start_border_length+len(str(index+1))+2
         print(f'\n{blue('--- running test')}', yellow(index+1), blue('-'*start_border_length), '\n')
         with open(f'./tests/{test}.pls') as f:
             src = f.read()
@@ -31,3 +31,6 @@ def run_tests():
         print(cyan(' - expected output -'), f'\n{indent}', expected.replace('\n', f'\n{indent}'))
         print(cyan(' - actual output -'), f'\n{indent}', actual.replace('\n', f'\n{indent}'))
         print(blue('---'), green('test passed') if expected in actual else red('test failed'), blue('-'*end_border_length), '\n'*5)
+
+
+
